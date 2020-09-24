29c5acc5f482fa56bad177a215e27ddbeb6baf17
Ivanenko Andrii <ivanenko0.andrii@gmail.com>
2020-09-24 15:46:27 +0300

    Добавьте файлы проекта.

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть змiнну типу int:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введiть змiнну типу double:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введiть змiнну типу long:");
+            long c = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+
+        }
+    }
+}
